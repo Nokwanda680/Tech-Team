d690aebd1ee2188f21fafb955b13d4d66c23a64c
Vuyisa@97 <nomfulavuyisa@gmail.com>
2026-07-26 20:39:18 +0200

    ...

--- backend/findmyvibe_backend/findmyvibe_backend/settings.py
+++ backend/findmyvibe_backend/findmyvibe_backend/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "False"
 
 #ALLOWED_HOSTS = [h for h in os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(",") if h]
-ALLOWED_HOSTS =['https://find-my-vibe-student-accomodation-finder.onrender.com']
+ALLOWED_HOSTS = ['https://find-my-vibe-student-accomodation-finder.onrender.com']
 
 # Application definition
 
